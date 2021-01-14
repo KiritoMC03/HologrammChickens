7d613bc2a9cad4ffe4ffcfcfba06399ca38aa6aa
Kirill Dudnik <prog.me.2017@gmail.com>
2021-01-14 19:19:00 +0600

    Version 0.3! / Версия 0.3! - added a space scene; - models of planets; - script motion in orbits in the shape of an ellipse; - orbits + their display via LineRenderer; - added a fallback option with real gravity -- unstable, planets often collide and it is impossible to make a constant orbit; ------------------------ - добавил сцену космоса; - модели планет; - скрипты движения по орбитам в форме элипса; - орбиты + их отображение через LineRenderer; - добавил запасной вариант с реальной гравитацией -- нестабильно, планеты часто сталкиваются и сделать постоянную орбиту невозможно;

---
+++ Assets/Scripts/Space/OrbitRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(OrbitMovement))]
+public class OrbitRenderer : MonoBehaviour
+{
+    /*[SerializeField]*/ private Orbit _orbit;
+    [Range(30, 100)]
+    [SerializeField] private int _segments;
+    private LineRenderer _lineRenderer;
+    private OrbitMovement _orbitMovement;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _orbitMovement = GetComponent<OrbitMovement>();
+        _orbit = _orbitMovement.orbitPath;
+        CalculateEllipse();
+    }
+
+    void CalculateEllipse()
+    {
+        Vector3[] points = new Vector3[_segments + 1];
+
+        for (int i = 0; i < _segments; i++)
+        {
+            Vector2 position2D = _orbit.Evaluate(i / (float)_segments);
+            points[i] = new Vector3(position2D.x, 0f, position2D.y) + transform.position;
+        }
+
+        points[_segments] = points[0];
+
+        _lineRenderer.positionCount = _segments + 1;
+        _lineRenderer.SetPositions(points);
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying)
+        {
+            CalculateEllipse();
+        }
+    }
+}
---
+++ Assets/Scripts/Space/PlanetGravity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class PlanetGravity : MonoBehaviour
+{
+    [SerializeField] private GravityZone _gravityZone;
+    private Rigidbody _rigidbody;
+    private const float G = 9.8f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        if (_gravityZone == null)
+        {
+            throw new Exception("GravityZone не установлен.");
+        }
+        else
+        {
+            _gravityZone = _gravityZone.GetComponent<GravityZone>();
+        }
+
+        if(_rigidbody != null)
+        {
+            _rigidbody.useGravity = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        CheckAffectedBodies(_gravityZone.AffectedBodies);
+    }
+
+    private void CheckAffectedBodies(HashSet<Rigidbody> affectedBodies)
+    {
+        foreach (Rigidbody body in affectedBodies)
+        {
+            var toBody = (transform.position - body.position);
+            var direction = toBody.normalized;
+            var distance = toBody.magnitude;
+            var force = G * body.mass * _rigidbody.mass / distance * distance;
+
+            body.AddForce(direction * force);
+        }
+    }
+}
