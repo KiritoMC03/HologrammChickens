2daf5e99e35136a5b482c83296f5775f6bc49d09
Kirill Dudnik <prog.me.2017@gmail.com>
2021-01-24 20:00:54 +0600

    Version 0.8! / Версия 0.8! - a variant of the Mars scene with a spherical object; - attraction of objects to the center of the planets; - player movement on the planet; -------------------------------- - вариант сцены марса со сферическим объектом; - притяжение объектов к центру планет; - движение игрока по планете;

---
+++ Assets/Scripts/Environment/LimitSlope.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitSlope : MonoBehaviour
+{
+    [SerializeField] internal Transform targetPlanet;
+
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(-_transform.up, targetPlanet.position - _transform.position);
+        _transform.rotation = rotation * _transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Environment/Planet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Planet : MonoBehaviour
+{
+    private HashSet<Rigidbody> _affectedBodies = new HashSet<Rigidbody>();
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+
+    private Vector3 _toPlanet = Vector3.zero;
+
+    void Start()
+    {
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var body in _affectedBodies)
+        {
+            _toPlanet = _transform.position - body.position;
+
+            float distance = _toPlanet.magnitude;
+            float strength = body.mass * _rigidbody.mass / distance;
+
+            body.AddForce(_toPlanet.normalized * strength);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+        {
+            Debug.Log(other);
+            _affectedBodies.Add(other.attachedRigidbody);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            _affectedBodies.Remove(other.attachedRigidbody);
+        }
+    }
+}
